9325f770f4738d98ef63a9eefa8235852bdf2e8f
ibaiv <ibaiv@PortatilDeIbai>
2023-04-25 17:20:01 +0200

    Pojo Tickets hecho

--- LosNanos/src/Pojos/ZooTicket/Ticket.java
+++ LosNanos/src/Pojos/ZooTicket/Ticket.java
@@ -1,11 +1,54 @@
 package Pojos.ZooTicket;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
 public class Ticket implements Serializable{
 
 
 	private static final long serialVersionUID = 2537052963906824962L;
 	private int id = 0;
+	private Date buyDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getBuyDate() {
+		return buyDate;
+	}
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(buyDate, id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ticket other = (Ticket) obj;
+		return Objects.equals(buyDate, other.buyDate) && id == other.id;
+	}
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", buyDate=" + buyDate + "]";
+	}
+	
+	
 
+	
+	
 }
