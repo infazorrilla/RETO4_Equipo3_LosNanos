eb53e4de976c07833a2ee65d2e36dd241068ba24
ibaiv <ibaiv@PortatilDeIbai>
2023-04-25 17:15:45 +0200

    Añadido Utils

---
+++ LosNanos/src/Pojos/ZooTicket/Ticket.java
@@ -0,0 +1,11 @@
+package Pojos.ZooTicket;
+
+import java.io.Serializable;
+
+public class Ticket implements Serializable{
+
+
+	private static final long serialVersionUID = 2537052963906824962L;
+	private int id = 0;
+
+}
--- LosNanos/src/Pojos/ZooTicket/Zoo.java
+++ LosNanos/src/Pojos/ZooTicket/Zoo.java
@@ -1,6 +1,5 @@
 package Pojos.ZooTicket;
 
-import java.util.Date;
 import java.util.Objects;
 
 public class Zoo{
@@ -10,53 +9,54 @@ private int id;
 private String name;
 private String location;
 
-
-public int setId (int id) {
-return id;
-}
-
-public String getName (String name) {
-return name;
-}
-
-public String getLocation (String location) {
-return location;
-}
-
-public String getName() {
-return name;
-}
-
-public void setName(String name) {
-this.name = name;
-}
-
-public String getLocation() {
-return location;
-}
-
-public void setLocation(String location) {
-this.location = location;
-}
-
-public static long getSerialversionuid() {
-return serialVersionUID;
-}
-
-public Zoo(int id, String name, String location) {
-this.id = id;
-this.name = name;
-this.location = location;
-}
-
 public int getId() {
-return id;
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public static long getSerialversionuid() {
+	return serialVersionUID;
+}
+
+
+@Override
+public int hashCode() {
+	return Objects.hash(id, location, name);
+}
+
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Zoo other = (Zoo) obj;
+	return id == other.id && Objects.equals(location, other.location) && Objects.equals(name, other.name);
 }
 
 @Override
 public String toString() {
-return "Zoo [id=" + id + ", name=" + name + ", location=" + location + ", getClass()=" + getClass()
-+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	return "Zoo [id=" + id + ", name=" + name + ", location=" + location + "]";
 }
+
+
+
+
 
 }
