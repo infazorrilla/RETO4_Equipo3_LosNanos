cfc729c51e0f9ee8a9173277b0654f5d5a0f5a63
pc <pc@Jonion>
2023-04-28 19:36:42 +0200

    f

--- LosNanos/src/Tests/ManagerTicketTest.java
+++ LosNanos/src/Tests/ManagerTicketTest.java
@@ -1,4 +1,4 @@
-package Manager;
+package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- LosNanos/src/Tests/ManagerZooTest.java
+++ LosNanos/src/Tests/ManagerZooTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Pojos.ZooTicket.Zoo;
+
 class ManagerZooTest {
-
+	
+	Zoo zoo = new Zoo();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void selectZoo() {
+		
 	}
-
+	
 }
